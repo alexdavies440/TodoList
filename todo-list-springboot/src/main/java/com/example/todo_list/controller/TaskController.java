543248dc8c94c396264d8d9c68718376de5ac42d
Alex <alex@Alexs-MacBook-Air.local>
2025-02-09 23:15:19 -0600

    added mapping in preparation for add and delete task buttons

--- todo-list-springboot/src/main/java/com/example/todo_list/controller/TaskController.java
+++ todo-list-springboot/src/main/java/com/example/todo_list/controller/TaskController.java
@@ -3,11 +3,10 @@ package com.example.todo_list.controller;
 import com.example.todo_list.data.TaskRepository;
 import com.example.todo_list.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -19,5 +18,16 @@ public class TaskController {
     @GetMapping("tasks")
     public List<Task> sendTask() {
         return taskRepository.findAll();
+    }
+
+    @PostMapping("delete")
+    public void deleteTask(@RequestParam int id) {
+        taskRepository.deleteById(id);
+    }
+
+    @PostMapping("add")
+    public void addTask(@RequestParam int id) {
+        Optional<Task> newTask = taskRepository.findById(id);
+        newTask.ifPresent(task -> taskRepository.save(task));
     }
 }
